8eae26f97ba4413fa7392531504a54c0d2287318
biedro94 <biedrzyckid94@gmail.com>
2017-02-24 21:40:19 +0100

    Tworzenie struktury projektu backe i front end

---
+++ MyRoots/Js/Implementation/Home.js
@@ -0,0 +1,10 @@
+"use strict";
+require('../../Scripts/typings/knockout/knockout.d.ts');
+require('../../Scripts/typings/jquery/jquery.d.ts');
+var Home = (function () {
+    function Home() {
+    }
+    return Home;
+}());
+exports.Home = Home;
+//# sourceMappingURL=Home.js.map
---
+++ MyRoots/Js/Implementation/Home.ts
@@ -0,0 +1,9 @@
+﻿import '../../Scripts/typings/knockout/knockout.d.ts';
+import '../../Scripts/typings/jquery/jquery.d.ts';
+
+
+export class Home {
+
+
+
+}
