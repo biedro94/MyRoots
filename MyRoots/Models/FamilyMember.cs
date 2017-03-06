6accaca2587dd7a2b3b08cb11965f531d71257ee
walery1409 <pawel140923@gmail.com>
2017-03-06 18:13:16 +0100

    Added Models, enabled migrations

---
+++ MyRoots/Models/FamilyMember.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyRoots.Models
+{
+    public class FamilyMember
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfDeath { get; set; }
+        public string BirthPlace { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+        public virtual Tree Tree { get; set; }
+    }
+}
